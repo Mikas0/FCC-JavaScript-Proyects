37f10b4438dc216aafe71a017d925956e2a8dafa
Mikas0 <miguelmd09@gmail.com>
2020-08-22 12:58:44 -0400

    convertidor de numeros romanos operativo

--- convertidor de números romanos/convertidorRomano.js
+++ convertidor de números romanos/convertidorRomano.js
@@ -1,4 +1,5 @@
-function convertToRoman(num) {
+function convertToRoman() {
+  let num = document.getElementById("ubarra").value;
     var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
    var romanNumeral = [
      "M",
