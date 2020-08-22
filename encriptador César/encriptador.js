4907e6a096f2ddcce116677940db2eb3a7ba358c
Mikas0 <miguelmd09@gmail.com>
2020-08-22 13:46:16 -0400

    encriptador funciona mejor, detalles en archivo de texto actualizado

--- encriptador César/encriptador.js
+++ encriptador César/encriptador.js
@@ -7,16 +7,20 @@ function rot13() {
          .map.call(str, function(char) {
            // Convert char to a character code
            var x = char.charCodeAt(0);
-           // Checks if character lies between A-Z
-           if (x < 65 || x > 90) {
+           // Checks if character lies between A-Z, a-z, and also checks for [], \, ^, _ and `
+           if (x < 65 || x > 122) {
              return String.fromCharCode(x); // Return un-converted character
            }
-           //N = ASCII 78, if the character code is less than 78, shift forward 13 places
-           else if (x < 78) {
+           //[ = ASCII 91, if the character code is less than 91, shift backward 13 places
+           else if (x < 91) {
              return String.fromCharCode(x + 13);
            }
-           // Otherwise shift the character 13 places backward
-           return String.fromCharCode(x - 13);
+           // Otherwise shift the character 13 places forward
+           else if (x > 96){
+             return String.fromCharCode(x - 13);
+           }
+           // If between ASCII 91 & 96 then leave as is
+           else return String.fromCharCode(x);
          })
          // Rejoin the array into a string
          .join("");
